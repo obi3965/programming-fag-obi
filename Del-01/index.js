350b44fdabeb4eabf575b48b12e8f3083f3bf45e
obi <obi@gmail.com>
2019-05-09 09:14:26 +0200

    commit

--- Del-01/index.js
+++ Del-01/index.js
@@ -1,6 +1,13 @@
-document.addEventListener('DOMContentLoaded', (event)=>{
+document.addEventListener("DOMContentLoaded", ()=>{
 
-    let switchsystem1 = new SwitchSystem ("#switchsystem-1");
-    let switchsystem2 = new SwitchSystem ("#switchsystem-2");
+	let switchSystem1 = new SwitchSystem ("#switchsystem-1");
+	let switchSystem2 = new SwitchSystem ("#switchsystem-2");
 
-})
+	console.log(switchSystem1.state);
+	switchSystem1.changeState();
+	console.log(switchSystem1.state);
+	switchSystem1.changeState();
+	console.log(switchSystem1.state);
+	switchSystem1.changeState();
+
+});
--- Del-01/switchsystem.js
+++ Del-01/switchsystem.js
@@ -1,34 +1,47 @@
-class switchSystem {
-    
-    constructor(cssSelector){
-       this.state = 0;
-      this.cssClassOff = "switchsystem-state-off";
-      this.cssClassOn = "switchsystem-state-on";
-      this.textOff = "Off"; 
-      this.textOn = "On";
-      this.parentElement = null;
-      this.buttonElement = null;
+class SwitchSystem {
+	constructor (cssSelector) {
+		this.state = 0;
+		this.cssClassOff = "switchsystem-state-off";
+		this.cssClassOn = "switchsystem-state-on";
+		this.textOff = "Off";
+		this.textOn = "On";
+		this.BtnElement = null;
 
-      this.attachToHTML(cssSelector)
-    }
-    changeState(){
-        if(this.state == 0){
-            this.state = 1;
-        }else{
+		this.attachToHTML (cssSelector);
+	}
 
-        }
-    }
+	changeState () {
+		if(this.state === 0){
+			this.state = 1;
+		}else{
+			this.state = 0;
+		}
+	}
+	
+	updateUI () {
+		if(this.state === 1){
+			this.BtnElement.innerHTML = this.textOn;
+			this.BtnElement.classList.add (this.cssClassOn);
+			this.BtnElement.classList.remove (this.cssClassOff);
+		}else{
+			this.BtnElement.innerHTML = this.textOff;
+			this.BtnElement.classList.add (this.cssClassOff);
+			this.BtnElement.classList.remove (this.cssClassOn);
 
+		}
+	}
+	
+	attachToHTML (cssSelector) {
+		this.BtnElement = document.querySelector (cssSelector + " button");
+		this.textOffElement = document.querySelector (cssSelector + " button");
+		
+		
+		this.BtnElement.addEventListener ("click", (event) => {
+			this.changeState();
+			this.updateUI();
+		});
 
-    updateUI(){
-        if(this.state == 1){
-            this.buttonElement.innerHTML = this.textOn;
-            this.buttonElement.classList.add()
-        }
-        this.buttonElement.innerHTML = "OFF"
-    }
+		this.updateUI();
+	}
 
-    attachToHTML(cssSelector){
-
-    }
 }
